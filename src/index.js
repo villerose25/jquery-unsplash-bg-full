110ddd78343962ff943989d0f3daa30bf6971aa0
Deyanira <deyanirad@hexaware.com>
2018-04-21 15:03:52 -0500

    add jquery env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-var sayHi = 'Hola mundo';
+/**
+ * jquery-unsplash-bg-full
+ * @version 0.1.0-development
+ * @author Deyanira Davila
+ * @license The MIT License (MIT)
+ */
+
+(function($) {
+    $.fn.setRed = function() {
+        this.css('background-color', 'red');
+        return this;
+    };
+})(jQuery);
